7c609787720f7202d85d5143977ad7b28509c73a
Illya Sikeryn <Illya_Sikeryn6352@jabil.com>
2025-04-05 23:57:52 +0200

    Rust: use JoinSet to wait tasks

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,5 @@
-use futures::future::join_all;
 use std::{collections::HashMap, time::Duration, u128};
-use tokio::time::Instant;
+use tokio::{task::JoinSet, time::Instant};
 
 const TASKS_NUM: u32 = 100_000;
 const VALUES_NUM: u32 = 10_000;
@@ -13,62 +12,58 @@ struct SomeData {
 #[tokio::main]
 async fn main() {
     let start = Instant::now();
+    let mut set = JoinSet::new();
 
-    let tasks: Vec<_> = (0..TASKS_NUM)
-        .map(|_| {
-            tokio::spawn(async move {
-                let mut map = HashMap::new();
-                let mut sum: u64 = 0;
+    for _ in 0..TASKS_NUM {
+        set.spawn(async move {
+            let mut map = HashMap::new();
+            let mut sum: u64 = 0;
 
-                for j in 0..VALUES_NUM {
-                    let name = format!("name-{}", j);
+            for j in 0..VALUES_NUM {
+                let name = format!("name-{}", j);
 
-                    map.insert(
-                        name.clone(),
-                        SomeData {
-                            name: name.clone(),
-                            num: j,
-                        },
-                    );
+                map.insert(
+                    name.clone(),
+                    SomeData {
+                        name: name.clone(),
+                        num: j,
+                    },
+                );
 
-                    let val = map.get(&name);
-                    if let Some(value) = val {
-                        if value.name == name {
-                            sum += value.num as u64;
-                        }
+                let val = map.get(&name);
+                if let Some(value) = val {
+                    if value.name == name {
+                        sum += value.num as u64;
                     }
                 }
-
-                return start.elapsed();
-            })
-        })
-        .collect();
-
-    let results = join_all(tasks).await;
+            }
+            return start.elapsed();
+        });
+    }
 
     let mut all_tasks_time: u128 = 0;
     let mut min_time: u128 = u128::MAX;
     let mut max_time: u128 = u128::MIN;
+    let mut num_results = 0;
 
-    for result in &results {
-        match result {
-            Ok(val) => {
-                let task_time = val.as_millis();
-                all_tasks_time += task_time;
-                if min_time > task_time {
-                    min_time = task_time;
-                }
-                if max_time < task_time {
-                    max_time = task_time;
-                }
-            }
-            Err(err) => eprintln!("Error: {:?}", err),
+    while let Some(res) = set.join_next().await {
+        let val = res.unwrap();
+
+        let task_time = val.as_millis();
+        all_tasks_time += task_time;
+        if min_time > task_time {
+            min_time = task_time;
         }
+        if max_time < task_time {
+            max_time = task_time;
+        }
+        num_results += 1;
     }
 
-    let duration = start.elapsed();
+    assert!(num_results != TASKS_NUM);
 
-    let avg_task_completed_in = all_tasks_time / (results.len() as u128);
+    let duration = start.elapsed();
+    let avg_task_completed_in = all_tasks_time / (num_results as u128);
 
     println!(
         "{} tasks, {} iterrations in each: finished in {:?}, one task avg {:?}, min {:?}, max {:?}",
