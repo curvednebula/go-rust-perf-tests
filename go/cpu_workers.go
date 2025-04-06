2a2e05fcd3c24b537a1c0f4fbf65ab07b24fd8c7
Illya Sikeryn <Illya_Sikeryn6352@jabil.com>
2025-04-06 15:55:50 +0200

    go: commets

--- go/cpu_workers.go
+++ go/cpu_workers.go
@@ -10,13 +10,16 @@ type cpuWorkers[T any] struct {
 }
 
 func NewCpuWorkers[T any]() *cpuWorkers[T] {
+	workersNum := runtime.NumCPU()
+
 	w := &cpuWorkers[T]{
-		sem:        make(chan struct{}, runtime.NumCPU()),
-		NumWorkers: runtime.NumCPU(),
+		sem:        make(chan struct{}, workersNum),
+		NumWorkers: workersNum,
 	}
 	return w
 }
 
+// will block until free CPU thread is available to execute the workFn()
 func (w *cpuWorkers[T]) DoWork(workFn func() T) T {
 	w.sem <- struct{}{} // acquire slot
 	ch := make(chan T, 1)
